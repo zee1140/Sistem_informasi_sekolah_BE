f6fc702073991431bd91fe305df1c79f5008f464
rifqimaul147 <rifqimaull478@gmail.com>
2026-04-28 11:58:08 +0700

    update

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -4,16 +4,26 @@ const AppError = require('../errors/AppError')
 module.exports = (req, res, next) => {
     const authHeader = req.headers.authorization
 
+    console.log('HEADER:', authHeader) // 🔍 cek header masuk
+
     if (!authHeader) {
         return next(new AppError('TOKEN_REQUIRED', 401))
     }
 
     const token = authHeader.split(' ')[1]
 
+    console.log('TOKEN:', token) // 🔍 cek token kebaca
+    console.log('SECRET:', process.env.JWT_SECRET) // 🔍 cek env kebaca
+
     try {
-        req.user = jwt.verify(token, 'SECRET_KEY') // inject user
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        console.log('DECODED:', decoded) // 🔍 cek isi token
+
+        req.user = decoded
         next()
     } catch (err) {
+        console.log('JWT ERROR:', err.message) // 🔥 ini penting
         next(new AppError('INVALID_TOKEN', 401))
     }
 }

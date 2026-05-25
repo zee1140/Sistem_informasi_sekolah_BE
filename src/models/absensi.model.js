9cb3c619bdb502bcce9b5fe0dc50ea886526edb8
rifqimaul147 <rifqimaull478@gmail.com>
2026-05-25 15:08:42 +0700

    new update

--- src/models/absensi.model.js
+++ src/models/absensi.model.js
@@ -2,22 +2,43 @@ const db = require('../config/db')
 
 // GET ALL
 exports.findAll = async () => {
-  const [rows] = await db.query(
-    `SELECT * FROM absensi
-     WHERE deleted_at IS NULL`
-  )
+  const [rows] = await db.query(`
+    SELECT
+      absensi.id,
+      absensi.siswa_id,
+      siswa.nama AS nama_siswa,
+      siswa.kode_kelas,
+      absensi.tanggal,
+      absensi.status,
+      absensi.created_at,
+      absensi.updated_at
+    FROM absensi
+    JOIN siswa
+      ON siswa.id = absensi.siswa_id
+    WHERE absensi.deleted_at IS NULL
+  `)
 
   return rows
 }
 
 // GET BY ID
 exports.findById = async (id) => {
-  const [rows] = await db.query(
-    `SELECT * FROM absensi
-     WHERE id = ?
-     AND deleted_at IS NULL`,
-    [id]
-  )
+  const [rows] = await db.query(`
+    SELECT
+      absensi.id,
+      absensi.siswa_id,
+      siswa.nama AS nama_siswa,
+      siswa.kode_kelas,
+      absensi.tanggal,
+      absensi.status,
+      absensi.created_at,
+      absensi.updated_at
+    FROM absensi
+    JOIN siswa
+      ON siswa.id = absensi.siswa_id
+    WHERE absensi.id = ?
+    AND absensi.deleted_at IS NULL
+  `, [id])
 
   return rows[0]
 }
@@ -31,12 +52,21 @@ exports.create = async (data) => {
     status
   } = data
 
-  await db.query(
-    `INSERT INTO absensi
-    (id, siswa_id, tanggal, status)
-    VALUES (?, ?, ?, ?)`,
-    [id, siswa_id, tanggal, status]
-  )
+  await db.query(`
+    INSERT INTO absensi
+    (
+      id,
+      siswa_id,
+      tanggal,
+      status
+    )
+    VALUES (?, ?, ?, ?)
+  `, [
+    id,
+    siswa_id,
+    tanggal,
+    status
+  ])
 }
 
 // UPDATE
@@ -47,23 +77,28 @@ exports.update = async (id, data) => {
     status
   } = data
 
-  await db.query(
-    `UPDATE absensi
-     SET
-       siswa_id = ?,
-       tanggal = ?,
-       status = ?
-     WHERE id = ?`,
-    [siswa_id, tanggal, status, id]
-  )
+  await db.query(`
+    UPDATE absensi
+    SET
+      siswa_id = ?,
+      tanggal = ?,
+      status = ?,
+      updated_at = NOW()
+    WHERE id = ?
+  `, [
+    siswa_id,
+    tanggal,
+    status,
+    id
+  ])
 }
 
-// DELETE
+// DELETE (SOFT DELETE)
 exports.delete = async (id) => {
-  await db.query(
-    `UPDATE absensi
-     SET deleted_at = NOW()
-     WHERE id = ?`,
-    [id]
-  )
+  await db.query(`
+    UPDATE absensi
+    SET
+      deleted_at = NOW()
+    WHERE id = ?
+  `, [id])
 }
